8cf7783318580048e235fdb39c3a49272490ae59
Aditya Raj Gupta <aditya9199034744@gmail.com>
2024-08-25 18:10:50 +0530

    sumOFTwoNumbers

---
+++ src/EvenOdd.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a num: ");
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("is even:" + num);
+        }else{
+            System.out.println("is odd:" + num);
+        }
+    }
+}
